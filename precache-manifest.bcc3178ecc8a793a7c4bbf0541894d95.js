d756f63c9b341e31cb0f5f4a5d7ba5236e74c33b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-24 23:21:32 +0000

    Deploy captbaritone/webamp to github.com/captbaritone/webamp.git:gh-pages

---
+++ precache-manifest.bcc3178ecc8a793a7c4bbf0541894d95.js
@@ -0,0 +1,81 @@
+self.__precacheManifest = [
+  {
+    "url": "/images/preview-42304ea56f49c60e3d2c6c97d1bdddeb.png"
+  },
+  {
+    "url": "/images/manifest/icon_192x192.ec9b715176586bf58ec5abe0354efd85.png"
+  },
+  {
+    "url": "/jsmediatags-0f00955453fb9ddfafe9.js"
+  },
+  {
+    "url": "/jsmediatags-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/milkdrop-preset-converter-0f00955453fb9ddfafe9.js"
+  },
+  {
+    "url": "/milkdrop-preset-converter-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/jszip-0f00955453fb9ddfafe9.js"
+  },
+  {
+    "url": "/jszip-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/images/manifest/icon_512x512.5228b937afc3368434b594aa65be2c08.png"
+  },
+  {
+    "url": "/images/manifest/icon_513x513.1f27cb4bba614c00bf595696efa4435d.svg"
+  },
+  {
+    "url": "/webamp-0f00955453fb9ddfafe9.js"
+  },
+  {
+    "url": "/webamp-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/manifest.60fc98cc18ea0b3ab073cda74610efa1.json"
+  },
+  {
+    "revision": "4d7623c1927950523c96cbca13c16e8f",
+    "url": "/index.html"
+  },
+  {
+    "url": "/images/share/butterchurn-bg-d09543a6c6d8150469b722454c583c07.png"
+  },
+  {
+    "url": "/css/page-54c8649f481ee783bf07e215c57f962d.css"
+  },
+  {
+    "url": "/images/favicon-0cc27932e7d5cbda2849b27e6e6dc3c8.ico"
+  },
+  {
+    "url": "/images/manifest/icon-72x72-9d9aaf3f4dad41359616e32e082dc71c.png"
+  },
+  {
+    "url": "/images/manifest/icon-96x96-44fece775401fd6184d71022778b4edd.png"
+  },
+  {
+    "url": "/images/manifest/icon-192x192-fd139d3437e0cad44d05f85b226a24d7.png"
+  },
+  {
+    "url": "/images/manifest/icon-48x48-67470808290acc080f2d154fd1df70d1.png"
+  },
+  {
+    "url": "/images/manifest/icon-144x144-06bb87f22f8080f239f096e856435678.png"
+  },
+  {
+    "url": "/butterchurn-presets-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/butterchurn-presets-0f00955453fb9ddfafe9.js"
+  },
+  {
+    "url": "/butterchurn-0f00955453fb9ddfafe9.js.map"
+  },
+  {
+    "url": "/butterchurn-0f00955453fb9ddfafe9.js"
+  }
+];
